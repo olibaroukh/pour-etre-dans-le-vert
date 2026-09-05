558b927dbfb735bdeacad3caa388c86a68aa3dec
olibaroukh <oli.baroukh@gmail.com>
2026-09-05 08:37:28 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Ne met rien en cache pour offline (volontairement) afin de toujours servir
 // la dernière version disponible sur le réseau.
  
-const SW_VERSION = '2026.09.05-2';
+const SW_VERSION = '2026.09.05-3';
  
 self.addEventListener('install', (event) => {
   self.skipWaiting();
